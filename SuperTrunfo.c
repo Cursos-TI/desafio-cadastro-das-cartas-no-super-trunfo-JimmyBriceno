e77577e79921640debbbfea8d3cb0112102d65b9
JimmyBriceno <jimmysilva549@gmail.com>
2025-04-01 23:55:29 +0000

    Criando a entrada de dados

---
+++ SuperTrunfo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    // Lendo a primeira carta do usuário
+
+    printf("Digite um estado: \n ");
+    printf("Digite o código da carta: \n ");
+    printf("Digite o nome da cidade: \n ");
+    printf("Digite a população: \n ");
+    printf("Digite a Área (em km²): \n ");
+    printf("Digite o PIB: \n ");
+    printf("Digite o número de pontos turísticos: \n ");
+
+    // Lendo a segunda carta do usuário
+
+    printf("Digite o segundo estado: \n ");
+    printf("Digite o código da carta: \n ");
+    printf("Digite o nome da cidade: \n ");
+    printf("Digite a população: \n ");
+    printf("Digite a Área (em km²): \n ");
+    printf("Digite o PIB: \n ");
+    printf("Digite o número de pontos turísticos: \n ");
+
+
+
+
+
+
+
+
+
+}
+
